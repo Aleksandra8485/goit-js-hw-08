3924a098a08e145310e039206d59ce0487166dc2
Aleksandra8485 <118071817+Aleksandra8485@users.noreply.github.com>
2023-04-03 22:10:07 +0200

    change in 01 gallery js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,32 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+for (const item of galleryItems) {
+  galleryEl.insertAdjacentHTML(
+    'beforeend',
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+  );
+}
+
+galleryEl.addEventListener('click', event => {
+  event.preventDefault();
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`
+  );
+  instance.show();
+});
+
+console.log(galleryItems);
