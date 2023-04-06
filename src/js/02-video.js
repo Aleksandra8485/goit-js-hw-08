6b804ffd9be65711278db8c6b5923f7bee695305
Aleksandra8485 <118071817+Aleksandra8485@users.noreply.github.com>
2023-04-06 20:25:13 +0200

    task 2 changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+//import throttle from 'lodash.throttle';
 
+//tworzę nowy obiekt new Player(odtwarzacz wideo)przypisany do zmiennej player.
+//Właściwość 'vimeo-player' odnosi się do wartości id w HTML.
+const player = new Player('vimeo-player');
+
+//kod zapisujący czas odtwarzania w local storage
+player.on('timeupdate', () => {
+  localStorage.setItem('videoplayer-current-time', player.currentTime());
+});
+
+//zmienna currentTime odczytuje czas odtwarzania z local storage
+//jeśli zmienna currentTime jest zdefiniowana to
+//player.setCurrentTime(currentTime)- ustawia czas odtwarzania
+//zapisuje czas zatrzymania i od niego wznawia
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
